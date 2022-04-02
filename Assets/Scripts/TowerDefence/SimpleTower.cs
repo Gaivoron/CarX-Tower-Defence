a067385b4b34ed8b6ccbb4020efa1fb9013bcf48
SergeyBenderskiy <hagal-the-greedy@yandex.ru>
2022-04-02 14:38:22 +0300

    Классы CannonTower и SimpleTower унаследованы от единого базового класса.

--- Assets/Scripts/TowerDefence/SimpleTower.cs
+++ Assets/Scripts/TowerDefence/SimpleTower.cs
@@ -3,35 +3,20 @@ using UnityEngine;
 
 namespace TowerDefence
 {
-	public class SimpleTower : MonoBehaviour
+    public sealed class SimpleTower : ShootingTower
 	{
-		public float m_shootInterval = 0.5f;
-		public float m_range = 4f;
-		public GameObject m_projectilePrefab;
+		[SerializeField]
 
-		private float m_lastShotTime = -0.5f;
+		private GameObject m_projectilePrefab;
 
-		void Update()
-		{
+        protected override void Shoot(Monster target)
+        {
 			if (m_projectilePrefab == null)
 				return;
 
-			foreach (var monster in FindObjectsOfType<Monster>())
-			{
-				if (Vector3.Distance(transform.position, monster.transform.position) > m_range)
-					continue;
-
-				if (m_lastShotTime + m_shootInterval > Time.time)
-					continue;
-
-				// shot
-				var projectile = Instantiate(m_projectilePrefab, transform.position + Vector3.up * 1.5f, Quaternion.identity);
-				var projectileBeh = projectile.GetComponent<GuidedProjectile>();
-				projectileBeh.m_target = monster.gameObject;
-
-				m_lastShotTime = Time.time;
-				break;
-			}
+			var projectile = Instantiate(m_projectilePrefab, transform.position + Vector3.up * 1.5f, Quaternion.identity);
+			var projectileBeh = projectile.GetComponent<GuidedProjectile>();
+			projectileBeh.m_target = target.gameObject;
 		}
-	}
+    }
 }

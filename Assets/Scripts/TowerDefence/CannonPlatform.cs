ba429a92f80b9f577b7d40b3392e76b9f979907b
SergeyBenderskiy <hagal-the-greedy@yandex.ru>
2022-04-06 22:10:26 +0300

    Бэкап. Методы дихотомии расширенны для работы с функциями с невещественными значениями. Кэшируем параметры рассчётов, чтобы не считать их потом дважды.

--- Assets/Scripts/TowerDefence/CannonPlatform.cs
+++ Assets/Scripts/TowerDefence/CannonPlatform.cs
@@ -28,28 +28,28 @@ namespace TowerDefence
 
 			var mover = target.Mover;
 			var maxTime = mover.EstimatedTime;
-			var timing = new Dichotomy(GetShootingTiming, 0, maxTime).GetSolution(1f / 24);
-			if (!timing.HasValue)
+			var targeting = new Dichotomy<TargetData>(GetTargeyingData, data => data.Time - data.Flight, 0, maxTime).GetSolution(1f / 24);
+			if (!targeting.HasValue)
 			{
 				return null;
 			}
 
-			var forecastedPosition = mover.PredictPosition(timing.Value.Item1);
+			var forecastedPosition = targeting.Value.Item2.Point;
 			if (!IsWithinReach(forecastedPosition))
 			{
 				return null;
 			}
 
-			return new Solution(this, forecastedPosition);
+			return new Solution(this, targeting.Value.Item2);
 
-			float GetShootingTiming(float hitTime)
+			TargetData GetTargeyingData(float hitTime)
 			{
 				var predictedPosition = mover.Position;
 				//TODO - take into account actual position of m_shootPoint when cannon will be facing predicted point?
 				var flightTime = Vector3.Distance(m_shootPoint.position, predictedPosition) / m_projectilePrefab.Speed;
 
 				var preparationTime = hitTime - flightTime;
-				return preparationTime;
+				return new TargetData(predictedPosition, hitTime, flightTime);
 			}
 		}
 
@@ -59,25 +59,37 @@ namespace TowerDefence
 			Gizmos.DrawRay(m_shootPoint.position, m_shootPoint.forward * 100);
         }
 
-        private sealed class Solution : ISolution
+		private struct TargetData
+		{
+			public Vector3 Point { get; }
+			public float Time { get; }
+			public float Flight { get; }
+
+			public TargetData(Vector3 point, float time, float flight)
+			{
+				Point = point;
+				Time = time;
+				Flight = flight;
+			}
+		}
+
+
+		private sealed class Solution : ISolution
 		{
 			private readonly CannonPlatform m_canon;
-
-			private readonly float m_rotationY;
-			//private readonly float m_rotationX;
-
-			private readonly Vector3 m_predictedPosition;
+            private readonly TargetData m_data;
+            private readonly float m_rotationY;
+			private readonly float m_rotationX;
 
 			//TODO - turn predictedPosition into rotation deltas.
-			//TODO - pass float rotationDuration?
-			public Solution(CannonPlatform canon, Vector3 predictedPosition)
+			public Solution(CannonPlatform canon, TargetData data)
 			{
 				m_canon = canon;
-				var direction = predictedPosition - m_canon.transform.position;
+				m_data = data;
+				var direction = data.Point - m_canon.transform.position;
 				m_rotationY = Vector3.SignedAngle(m_canon.m_yRotor.forward, direction, Vector3.up);
 				//m_rotationX = -Vector3.SignedAngle(m_canon.m_xRotor.forward, direction, m_canon.m_xRotor.right);
 				//Debug.Log(m_rotationX);
-				m_predictedPosition = predictedPosition;
 			}
 
             async UniTask<bool> ISolution.ExecuteAsync()
@@ -86,7 +98,7 @@ namespace TowerDefence
 				//var rotationY = m_canon.m_yRotor.localRotation.eulerAngles;
 				//m_canon.m_xRotor.RotateAround(m_canon.m_xRotor.position, m_canon.m_xRotor.right, m_rotationX);
 				m_canon.m_yRotor.RotateAround(m_canon.m_yRotor.position, Vector3.up, m_rotationY);
-				m_canon.m_xRotor.LookAt(m_predictedPosition);
+				m_canon.m_xRotor.LookAt(m_data.Point);
 				Instantiate(m_canon.m_projectilePrefab, m_canon.m_shootPoint.position, m_canon.m_shootPoint.rotation);
 				return true;
 			}
--- Assets/Scripts/TowerDefence/Dichotomy.cs
+++ Assets/Scripts/TowerDefence/Dichotomy.cs
@@ -3,33 +3,36 @@ using UnityEngine;
 
 namespace TowerDefence
 {
-    public sealed class Dichotomy
+    public sealed class Dichotomy<T>
 	{
-		private readonly Func<float, float> m_func;
-        private readonly (float, float) m_point1;
-		private readonly (float, float) m_point2;
+		private readonly Func<float, T> m_func;
+        private readonly Func<T, float> m_selector;
 
-        public Dichotomy(Func<float, float> func, float start, float end)
-			: this(func, (start, func(start)), (end, func(end)))
+        private readonly (float, T) m_point1;
+		private readonly (float, T) m_point2;
+
+        public Dichotomy(Func<float, T> func, Func<T, float> selector, float start, float end)
+			: this(func, selector, (start, func(start)), (end, func(end)))
         {
         }
 
-		private Dichotomy(Func<float, float> func, (float, float) point1, (float, float) point2)
+		private Dichotomy(Func<float, T> func, Func<T, float> selector, (float, T) point1, (float, T) point2)
 		{
 			m_func = func;
+			m_selector = selector;
 
 			m_point1 = point1;
 			m_point2 = point2;
 		}
 
 		//TODO - add function value tolerance as well.
-        public (float, float)? GetSolution(float distanceTolerance)
+        public (float, T)? GetSolution(float distanceTolerance)
 		{
 			var x1 = m_point1.Item1;
 			var x2 = m_point2.Item1;
 
-			var y1 = m_point1.Item2;
-			var y2 = m_point2.Item2;
+			var y1 = m_selector(m_point1.Item2);
+			var y2 = m_selector(m_point2.Item2);
 
 			if (Mathf.Sign(y1) * Mathf.Sign(y2) > 0)
 			{
@@ -37,15 +40,15 @@ namespace TowerDefence
 			}
 
 			var middleX = (x1 + x2) * 0.5f;
-			var middleY = m_func(middleX);
-			var middlePoint = (middleX, middleY);
+			var middleValue = m_func(middleX);
+			var middlePoint = (middleX, middleValue);
 			if (x2 - x1 <= distanceTolerance)
 			{
 				return middlePoint;
 			}
 
-
-			var iteration = Mathf.Sign(y1) * Mathf.Sign(middleY) <= 0 ? new Dichotomy(m_func, m_point1, middlePoint) : new Dichotomy(m_func, middlePoint, m_point2);
+			var middleY = m_selector(middleValue);
+			var iteration = Mathf.Sign(y1) * Mathf.Sign(middleY) <= 0 ? new Dichotomy<T>(m_func, m_selector, m_point1, middlePoint) : new Dichotomy<T>(m_func, m_selector, middlePoint, m_point2);
 			return iteration.GetSolution(distanceTolerance);
 		}
 	}
